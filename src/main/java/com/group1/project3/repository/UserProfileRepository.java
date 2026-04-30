3b98d8bdb1d16eedc3537a399fde5c6134c2ceae
Brandon <brandon.z.cano@gmail.com>
2026-04-30 10:33:18 -0700

    Created layer classes, added appropriate annotations, added findByUser() method in profile repository

--- src/main/java/com/group1/project3/repository/UserProfileRepository.java
+++ src/main/java/com/group1/project3/repository/UserProfileRepository.java
@@ -1,11 +1,15 @@
 package com.group1.project3.repository;
 
+import com.group1.project3.entity.User;
 import com.group1.project3.entity.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserProfileRepository extends JpaRepository<UserProfile, UUID> {
+
+    Optional<UserProfile>  findByUser(User user);
 }
---
+++ src/main/java/com/group1/project3/service/UserProfileService.java
@@ -0,0 +1,7 @@
+package com.group1.project3.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserProfileService {
+}
